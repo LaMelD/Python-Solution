eb75c5ccc6173bbf1118f6ef2f6fc54b6d53ba39
LaMelD <54210310+LaMelD@users.noreply.github.com>
2019-12-03 18:34:23 +0900

    update

--- 12915.py
+++ 12915.py
@@ -1,4 +1,4 @@
-# 문자열 내 마음대로 정렬하기
+# Programmers :: 문자열 내 마음대로 정렬하기
 def solution(strings, n):
         # n번째로 인덱스로 정렬하고, 그 인덱스가 같다면 사전순으로 정렬
         strings.sort(key = lambda text : (text[n], text))
